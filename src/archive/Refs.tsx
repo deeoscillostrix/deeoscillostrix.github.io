1c42681c9dba373ef3bfe797416cd68aa37f52f3
Henry Heng <deezombiedude612@hotmail.com>
2025-12-09 01:55:23 +0800

    Archived and cleaned up Refs

--- src/archive/Refs.tsx
+++ src/archive/Refs.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { HandleClipboardCopy, MyModal } from ".";
+import { HandleClipboardCopy, MyModal } from "../components";
 import { artists } from "../data";
 
 const PaletteButtons = ({ palette }: { palette: string[] }) => {
@@ -130,6 +130,69 @@ const Refs = () => {
       artistUrl: artists.orlandofox.website,
     },
   ];
+
+  // return (
+  //   <>
+  //     <button
+  //       className="btn btn-secondary btn-outline m-2 transition-all ease-in-out hover:scale-110"
+  //       onClick={() => {
+  //         const modal = document.querySelector(
+  //           `#${modalId}`
+  //         ) as HTMLDialogElement;
+  //         modal!.showModal();
+  //       }}
+  //     >
+  //       {title}
+  //     </button>
+  //     <dialog id={modalId} className="modal">
+  //       <form method="dialog" className="modal-backdrop">
+  //         <button>close</button>
+  //       </form>
+  //       <div className={modalSizes.large}>
+  //         <form method="dialog">
+  //           {/* if there is a button in form, it will close the modal */}
+  //           <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+  //             ✕
+  //           </button>
+  //         </form>
+
+  //         <h3>{title}</h3>
+
+  //         {refImages.map((image, index) => (
+  //           <div key={index} className="galleryDisplay">
+  //             <div className="imgContainer">
+  //               <img src={image.src} alt={image.alt} loading="lazy" />
+  //             </div>
+  //             <p className="mt-2 mb-4">
+  //               {image.alt} by{" "}
+  //               <a
+  //                 href={image.artistUrl}
+  //                 target="_blank"
+  //                 rel="noopener"
+  //                 className="hover:text-primary"
+  //               >
+  //                 {image.artist}
+  //               </a>
+  //               <span className="ms-2 text-primary">
+  //                 <a
+  //                   href={image.download}
+  //                   target="_blank"
+  //                   className="btn btn-sm btn-accent text-white py-3! transition-all ease-in-out duration-150 hover:scale-110 focus:scale-110"
+  //                 >
+  //                   Download
+  //                 </a>
+  //               </span>
+  //             </p>
+  //             {"palette" in image && (
+  //               <PaletteButtons palette={image.palette!} />
+  //             )}
+  //             {index < refImages.length - 1 && <hr />}
+  //           </div>
+  //         ))}
+  //       </div>
+  //     </dialog>
+  //   </>
+  // );
 
   return (
     <>
